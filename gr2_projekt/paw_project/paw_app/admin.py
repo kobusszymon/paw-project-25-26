9fab36274fbb548e246d85c39d7d7b48b60d2cf2
kobusszymon <szymon.kobus@icloud.com>
2025-11-14 12:12:57 +0100

    lab 05 changes

--- gr2_projekt/paw_project/paw_app/admin.py
+++ gr2_projekt/paw_project/paw_app/admin.py
@@ -3,12 +3,11 @@ from django.contrib import admin
 from .models import Genre, Author, Book, Osoba, Stanowisko
 
 class OsobaAdmin(admin.ModelAdmin):
-    list_display = ("imie", "nazwisko", "plec", "stanowisko", "data_dodania")
-    list_filter = ("stanowisko", "data_dodania")
+    list_display = ["imie", "nazwisko", "plec", "stanowisko", "data_dodania"]
+    list_filter = ["stanowisko", "data_dodania"]
     
 class StanowiskoAdmin(admin.ModelAdmin):
-    list_display = ("nazwa", "opis")
-    list_filter = ("nazwa")
+    list_filter = ["nazwa"]
 
 admin.site.register(Genre)
 admin.site.register(Author)
